fdeb0032dd1471020d247983b0ce3b03fce3f65c
Taisiia <taisiiabb@gmail.com>
2024-07-10 20:21:23 -0500

    First commit - Hello world

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 19 ; i++) {
+            System.out.println("Batch "+ i + " is great");
+            Thread.sleep(2000);
+        }
+        System.out.println("But teh Batch 19 is the greatest!");
     }
 }
